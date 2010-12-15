7f031501d931b02e09937168b000087ab14dc170
lennart <lennart@augustsson.net>
2010-12-16 00:37:08 +0200

    More tests.
    Ignore-this: 5f3e4660caa254a45003de0e98da345
    
    darcs-hash:20101215223708-1afbd-1fff19fca27c769588b1b43af136b1b4b8bbd9a5

--- tests/Err7.hs
+++ tests/Err7.hs
@@ -2,7 +2,11 @@
 data T = T { x :: Int }
 data S = S { x :: Int }
 
-data U = U { y :: Int, y :: Int }
+data U = U { y :: Int, y :: Int }  -- XXX error not detected yet
 
 -- But this OK
---data X = X1 { a :: Int } | X2 { a :: Int }
+data X = X1 { a :: Int } | X2 { a :: Int }
+
+class C a where
+    m :: a
+    m :: a
