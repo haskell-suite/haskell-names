33d9ad27013664a3261f3f104d03174fc58e77d3
Roman Cheplyaka <roma@ro-che.info>
2012-11-29 03:02:01 +0200

    gen-iface (almost done)

--- gen-iface.hs
+++ gen-iface.hs
@@ -1,49 +1,77 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import Language.Haskell.Exts.Annotated hiding (fromParseResult)
+import qualified Language.Haskell.Exts.Annotated as HSE
 import Language.Haskell.Modules
 import Language.Haskell.Modules.Interfaces
 import Language.Haskell.Modules.Flags
+import Language.Haskell.Modules.ModuleSummary
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Trans
 import Control.Exception
 import Data.Foldable
 import qualified Data.Map as Map
 import Data.Typeable
+import System.FilePath
 
-import Distribution.HaskellSuite.Simple
+import Distribution.HaskellSuite.Tool
+import Distribution.HaskellSuite.Cabal
+import Distribution.HaskellSuite.Helpers
 import Paths_haskell_modules
 
 data GenIfaceException
-  = ParseError SrcLoc String
+  = ParseError HSE.SrcLoc String
   | ScopeErrors [Msg]
+  | ModuleNotFound ModuleName
   deriving (Show, Typeable)
 
 instance Exception GenIfaceException
 
-fromParseResult :: ParseResult a -> IO a
-fromParseResult (ParseOk x) = return x
-fromParseResult (ParseFailed loc msg) = throwIO $ ParseError loc msg
+fromParseResult :: HSE.ParseResult a -> IO a
+fromParseResult (HSE.ParseOk x) = return x
+fromParseResult (HSE.ParseFailed loc msg) = throwIO $ ParseError loc msg
 
 main =
-  defaultMain =<<
-    simpleTool
-      "haskell-modules"
-      version
-      "/dev/null"
-      compile
-      ["names"]
+  defaultMain theTool
 
-compile buildDir files = do
+suffix :: String
+suffix = "names"
+
+theTool =
+  simpleTool
+    "haskell-modules"
+    version
+    (return "/dev/null")
+    compile
+    [suffix]
+
+compile buildDir pkgdbs pkgids files = do
   moduleSet <- forM files $ \file ->
-    return . fmap srcInfoSpan =<< fromParseResult =<< parseFile file
-  let (msgs, scopedModules) = scopeAnalysis defaultFlags moduleSet
-      errs = filter isError msgs
+    return . fmap HSE.srcInfoSpan =<< fromParseResult =<< HSE.parseFile file
+  let analysis = scopeAnalysis getModuleInfo' defaultFlags moduleSet
+  packages <- readPackagesInfo theTool pkgdbs pkgids
+  (msgs, scopedModules) <-
+    evalModuleT analysis packages retrieveModuleInfo Map.empty
+  let errs = filter isError msgs
   when (not $ null errs) $
     throwIO $ ScopeErrors errs
   undefined
 
+-- This functions works in the ModuleT monad and will be actually called
+-- by the scope analysis
+getModuleInfo' :: HSE.ModuleName () -> ModuleT ModuleSummary IO ModuleSummary
+getModuleInfo' (HSE.ModuleName _ modStr) = do
+  let modName = fromString modStr
+  mbModInfo <- getModuleInfo modName
+  maybe (liftIO $ throwIO $ ModuleNotFound modName) return mbModInfo
+
+-- This function says how we actually find and read the module
+-- information, given the search path and the module name
+retrieveModuleInfo :: [FilePath] -> ModuleName -> IO ModuleSummary
+retrieveModuleInfo dirs name = do
+  (base, rel) <- findModuleFile dirs [suffix] name
+  readInterface $ base </> rel
 
 {-
   mod <- fromParseResult <$> parseFile f
