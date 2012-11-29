6a2037e1a877e919295308e8a251ed28413bd209
Roman Cheplyaka <roma@ro-che.info>
2012-11-29 13:28:19 +0200

    Move symbolsToSumary to SymbolTable

--- src/Language/Haskell/Modules/Interfaces.hs
+++ src/Language/Haskell/Modules/Interfaces.hs
@@ -9,10 +9,7 @@ import qualified Data.ByteString.Lazy as BS
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Types as Aeson
 import Data.Aeson ((.:))
-import qualified Data.Map as Map
-import qualified Data.Set as Set
 import Data.Monoid
-import Data.List
 import Data.Char
 import Data.Typeable
 import Control.Exception
@@ -99,61 +96,3 @@ instance Aeson.FromJSON OrigName where
 
 instance Aeson.ToJSON (ModuleName a) where
   toJSON (ModuleName _ n) = Aeson.toJSON n
-
-data E =
-  E !(Set.Set ValueName)
-    !(Map.Map TypeName ([ValueName], ClassOrType))
-
-instance Monoid E where
-  mempty = E mempty mempty
-  mappend (E v1 t1) (E v2 t2) =
-    E (mappend v1 v2)
-      (Map.unionWith
-        (\(vs1,k1) (vs2,k2) ->
-          assert (k1 == k2) (vs1++vs2, k1))
-        t1 t2)
-
-makeIface :: (ModuleName (), Symbols) -> ModuleSummary
-makeIface (ModuleName _ n, (vs, ts)) =
-  ModuleSummary
-    { m_moduleName = n
-    , m_values = Set.toList vs'
-    , m_types = [ (t,v,k) | (t,(v,k)) <- Map.toList ts' ]
-    , m_fixities = []
-    }
-  where
-    E vs' ts' =
-      foldl' (\a x -> a `mappend` classifyT x) mempty ts `mappend`
-      foldl' (\a x -> a `mappend` classifyV x) mempty vs
-
-    classifyT :: SymTypeInfo -> E
-    classifyT v =
-      case v of
-        SymType t _    -> ty t t Type
-        SymData t _    -> ty t t Type
-        SymTypeFam t _ -> ty t t Type
-        SymDataFam t _ -> ty t t Type
-        SymClass t _   -> ty t t Class
-        SymTClash {}   -> error "Clash"
-
-    classifyV :: SymValueInfo -> E
-    classifyV v =
-      case v of
-        SymValue n _         -> value n
-        SymMethod n _ cls    -> ty cls n Class
-        SymSelector n _ t    -> ty t n Type
-        SymForeign n _       -> value n
-        SymConstructor n _ t -> ty t n Type
-        SymVClash {}         -> error "Clash"
-
-    value n = E (Set.singleton $ qNameToOrigName n) mempty
-    ty t v k =
-      E mempty
-        (Map.singleton (qNameToOrigName t)
-        ([qNameToOrigName v], k))
-
-qNameToOrigName :: QName a -> OrigName
-qNameToOrigName (Qual _ (ModuleName _ m) n) = OrigName m n'
-  where
-    n' = case n of Ident _ n' -> n'; Symbol _ n' -> n'
-qNameToOrigName _ = error "qNameToOrigName"
--- src/Language/Haskell/Modules/SymbolTable.hs
+++ src/Language/Haskell/Modules/SymbolTable.hs
@@ -3,22 +3,22 @@ module Language.Haskell.Modules.SymbolTable(
     SymFixity,
     SymValueInfo(..), sv_parent, SymValueInfos, symValueLookup, symValueAdd,
     SymTypeInfo(..), SymTypeInfos, symTypeLookup, symTypeAdd,
-    Symbols, symInfos, -- symMake, --symAssocs,
---    QU, toQU
+    Symbols, symInfos, symbolsToSumary
     ) where
-import qualified Data.Map as M
---import Control.Arrow((&&&), first)
-import Data.List(union)
+
+import Language.Haskell.Modules.ModuleSummary
 import Language.Haskell.Exts.Annotated
 import qualified Language.Haskell.Exts.Syntax as S
---import qualified Language.Haskell.Exts.Fixity as S
-
---import Language.Haskell.Modules.MonadModule(ModuleNameS, TypeName, ValueName, WithOrigName)
+import Data.Monoid
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.List
+import Control.Exception
 
 import Language.Haskell.Modules.SyntaxUtils(nameToString, specialConToString)
 
-data SymbolTable = T (M.Map QU SymValueInfo)
-                     (M.Map QU SymTypeInfo)
+data SymbolTable = T (Map.Map QU SymValueInfo)
+                     (Map.Map QU SymTypeInfo)
     deriving (Show)
 
 type SymFixity = (S.Assoc, Int)
@@ -74,35 +74,35 @@ type SymTypeInfos = [SymTypeInfo]
 type Symbols = (SymValueInfos, SymTypeInfos)
 
 symEmpty :: SymbolTable
-symEmpty = T M.empty M.empty
+symEmpty = T Map.empty Map.empty
 
 --symMerge :: SymbolTable -> SymbolTable -> SymbolTable
---symMerge (T vs ts) (T vs' ts') = T (M.union vs vs') (M.union ts ts')
+--symMerge (T vs ts) (T vs' ts') = T (Map.union vs vs') (Map.union ts ts')
 
 symValueLookup :: QName l -> SymbolTable -> Maybe SymValueInfo
-symValueLookup qn (T vs _) = M.lookup (toQU qn) vs
+symValueLookup qn (T vs _) = Map.lookup (toQU qn) vs
 
 symValueAdd :: QName l -> SymValueInfo -> SymbolTable -> SymbolTable
-symValueAdd qn i (T vs ts) = T (M.insertWith combineValue (toQU qn) i vs) ts
+symValueAdd qn i (T vs ts) = T (Map.insertWith combineValue (toQU qn) i vs) ts
 
 symTypeLookup :: QName l -> SymbolTable -> Maybe SymTypeInfo
-symTypeLookup qn (T _ ts) = M.lookup (toQU qn) ts
+symTypeLookup qn (T _ ts) = Map.lookup (toQU qn) ts
 
 symTypeAdd :: QName l -> SymTypeInfo -> SymbolTable -> SymbolTable
-symTypeAdd qn i (T vs ts) = T vs (M.insertWith combineType (toQU qn) i ts)
+symTypeAdd qn i (T vs ts) = T vs (Map.insertWith combineType (toQU qn) i ts)
 
 symShow :: SymbolTable -> String
-symShow (T vs ts) = unlines $ map (\ (i, _si) -> show i) (M.toList vs)
-                           ++ map (\ (i, _si) -> show i) (M.toList ts)
+symShow (T vs ts) = unlines $ map (\ (i, _si) -> show i) (Map.toList vs)
+                           ++ map (\ (i, _si) -> show i) (Map.toList ts)
 
 symInfos :: SymbolTable -> Symbols
-symInfos (T vs ts) = (M.elems vs, M.elems ts)
+symInfos (T vs ts) = (Map.elems vs, Map.elems ts)
 
 --symAssocs :: SymbolTable -> ([(QName (), SymTypeInfo)], [(QName (), SymValueInfo)])
---symAssocs (T vs ts) = (map (first . fromQU) $ M.assocs ts, map (first . fromQU) $ M.assocs vs)
+--symAssocs (T vs ts) = (map (first . fromQU) $ Map.assocs ts, map (first . fromQU) $ Map.assocs vs)
 
 --symMake :: SymInfos -> SymbolTable
---symMake (ts, vs) = T (M.fromListWith combineValue $ map (toQU . sv_origName &&& id) vs) (M.fromListWith combineType $ map (toQU . st_origName &&& id) ts)
+--symMake (ts, vs) = T (Map.fromListWith combineValue $ map (toQU . sv_origName &&& id) vs) (Map.fromListWith combineType $ map (toQU . st_origName &&& id) ts)
 
 combineValue :: SymValueInfo -> SymValueInfo -> SymValueInfo
 combineValue s1 (SymVClash o f ns) = SymVClash o f (union [sv_origName s1] ns)
@@ -117,3 +117,63 @@ combineType s1 s2 | n1 =~= n2 = s1
                   | otherwise = SymTClash (st_origName s1) (st_fixity s1) [n1, n2]
   where n1 = st_origName s1
         n2 = st_origName s2
+
+-- Conversion from and to ModuleSummary
+
+data E =
+  E !(Set.Set ValueName)
+    !(Map.Map TypeName ([ValueName], ClassOrType))
+
+instance Monoid E where
+  mempty = E mempty mempty
+  mappend (E v1 t1) (E v2 t2) =
+    E (mappend v1 v2)
+      (Map.unionWith
+        (\(vs1,k1) (vs2,k2) ->
+          assert (k1 == k2) (vs1++vs2, k1))
+        t1 t2)
+
+symbolsToSumary :: (ModuleName (), Symbols) -> ModuleSummary
+symbolsToSumary (ModuleName _ n, (vs, ts)) =
+  ModuleSummary
+    { m_moduleName = n
+    , m_values = Set.toList vs'
+    , m_types = [ (t,v,k) | (t,(v,k)) <- Map.toList ts' ]
+    , m_fixities = []
+    }
+  where
+    E vs' ts' =
+      foldl' (\a x -> a `mappend` classifyT x) mempty ts `mappend`
+      foldl' (\a x -> a `mappend` classifyV x) mempty vs
+
+    classifyT :: SymTypeInfo -> E
+    classifyT v =
+      case v of
+        SymType t _    -> ty t t Type
+        SymData t _    -> ty t t Type
+        SymTypeFam t _ -> ty t t Type
+        SymDataFam t _ -> ty t t Type
+        SymClass t _   -> ty t t Class
+        SymTClash {}   -> error "Clash"
+
+    classifyV :: SymValueInfo -> E
+    classifyV v =
+      case v of
+        SymValue n _         -> value n
+        SymMethod n _ cls    -> ty cls n Class
+        SymSelector n _ t    -> ty t n Type
+        SymForeign n _       -> value n
+        SymConstructor n _ t -> ty t n Type
+        SymVClash {}         -> error "Clash"
+
+    value n = E (Set.singleton $ qNameToOrigName n) mempty
+    ty t v k =
+      E mempty
+        (Map.singleton (qNameToOrigName t)
+        ([qNameToOrigName v], k))
+
+qNameToOrigName :: QName a -> OrigName
+qNameToOrigName (Qual _ (ModuleName _ m) n) = OrigName m n'
+  where
+    n' = case n of Ident _ n' -> n'; Symbol _ n' -> n'
+qNameToOrigName _ = error "qNameToOrigName"
