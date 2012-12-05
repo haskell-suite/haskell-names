a56fbfbf15178a8b6cfed93bc3c94c26a117ac29
Roman Cheplyaka <roma@ro-che.info>
2012-12-05 13:53:16 +0200

    Cut modules

--- src/Language/Haskell/Modules.hs
+++ src/Language/Haskell/Modules.hs
@@ -1,9 +1,2 @@
 module Language.Haskell.Modules(
-    module Language.Haskell.Modules.Error,
-    module Language.Haskell.Modules.Recursive,
-    module Language.Haskell.Modules.Scope
     ) where
-import Language.Haskell.Modules.Error
-import Language.Haskell.Modules.ModuleSummary
-import Language.Haskell.Modules.Recursive
-import Language.Haskell.Modules.Scope
