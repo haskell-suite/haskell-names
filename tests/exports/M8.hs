150b39faf63f8782d45014a719446da29f5726c5
Roman Cheplyaka <roma@ro-che.info>
2012-12-19 18:37:11 +0200

    M8

---
+++ tests/exports/M8.hs
@@ -0,0 +1,11 @@
+module M8 (Foo(Foo1, Foo3, c), Bar(x), N(N)) where
+
+data Foo = Foo1 | Foo2 { c :: Bool } | Foo3 { d :: Bool }
+
+class Bar where x :: Foo; y :: Foo
+
+newtype N = N { unN :: Foo }
+
+a = a
+
+data Baz = Baz
