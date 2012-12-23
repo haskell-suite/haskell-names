74a4f32fb8b024c4edd1a81f5b52ed70aa477d40
Roman Cheplyaka <roma@ro-che.info>
2012-12-23 14:15:19 +0200

    Library test module

---
+++ tests/exports/Library.hs
@@ -0,0 +1,17 @@
+module Library where
+
+data DataType = Constructor1 | Constructor2
+
+data DataTypeWithSelectors = DataTypeWithSelectors { selector1 :: DataTypeWithSelectors, selector2 :: DataTypeWithSelectors }
+
+newtype Newtype = Newtype Newtype
+
+newtype NewtypeWithSelectors = NewtypeWithSelectors { unNewtype :: NewtypeWithSelectors }
+
+type TypeSynonym = DataTypeWithSelectors
+
+class Class where
+  method1 :: DataTypeWithSelectors
+  method2 :: DataTypeWithSelectors
+
+function = function
