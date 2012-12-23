100699b9d9bc4fe955f0e4338e28dcdd00e98678
Roman Cheplyaka <roma@ro-che.info>
2012-12-23 18:02:07 +0200

    Import tests

--- test.hs
+++ test.hs
@@ -13,14 +13,16 @@ import Text.Show.Pretty
 import Language.Haskell.Exts.Annotated
 import Language.Haskell.Modules.Types
 import Language.Haskell.Modules.Exports
+import Language.Haskell.Modules.Imports
 import Language.Haskell.Modules.ModuleSymbols
+import Language.Haskell.Modules.SyntaxUtils
 import qualified Language.Haskell.Modules.GlobalSymbolTable as Global
 import Distribution.HaskellSuite.Helpers
 import qualified Distribution.ModuleName as Cabal
 
 main = defaultMain =<< tests
 
-tests = sequence [exportTests]
+tests = sequence [exportTests, importTests]
 
 parseAndPrepare file =
   return . fmap srcInfoSpan . fromParseResult =<< parseFile file
@@ -51,3 +53,26 @@ exportTests = do
   mods <- getModules
   testFiles <- find (return True) (extension ==? ".hs") "tests/exports"
   return $ testGroup "exports" $ map (exportTest mods) testFiles
+
+-- Import test: parse a source file, dump its global table
+importTest mods file =
+  goldenVsFile file golden out run
+  where
+    golden = file <.> "golden"
+    out = file <.> "out"
+    run = do
+      exps <- getInterface mods file
+      writeFile out $ ppShow exps
+
+getGlobalTable :: Map.Map Cabal.ModuleName Symbols -> FilePath -> IO Global.Table
+getGlobalTable mods file = do
+  mod <- parseAndPrepare file
+  let mImps = snd <$> processImports (getImports mod)
+      imps = runIdentity $
+        evalModuleT mImps [] (error "retrieve") mods
+  return imps
+
+importTests = do
+  mods <- getModules
+  testFiles <- find (return True) (extension ==? ".hs") "tests/imports"
+  return $ testGroup "imports" $ map (exportTest mods) testFiles
