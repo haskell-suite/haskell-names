b092b102ca4309bcdeaf2a58162cdc654e957d03
Roman Cheplyaka <roma@ro-che.info>
2012-12-23 14:33:26 +0200

    Provide Library module for inclusion

--- test.hs
+++ test.hs
@@ -14,30 +14,39 @@ import Control.Applicative
 import Text.Show.Pretty
 
 import Language.Haskell.Exts.Annotated
+import Language.Haskell.Modules.Types
 import Language.Haskell.Modules.Exports
 import Language.Haskell.Modules.ModuleSymbols
 import qualified Language.Haskell.Modules.GlobalSymbolTable as Global
 import Distribution.HaskellSuite.Helpers
+import qualified Distribution.ModuleName as Cabal
 
 main = defaultMain =<< tests
 
 tests = sequence [exportTests]
 
 -- Export test: parse a source file, dumb its symbols
-exportTest file =
+exportTest mods file =
   goldenVsFile file golden out run
   where
     golden = file <.> "golden"
     out = file <.> "out"
     run = do
+      exps <- getInterface mods file
+      BS.writeFile out $ BS.fromString $ ppShow exps
+
+getInterface :: Map.Map Cabal.ModuleName Symbols -> FilePath -> IO Symbols
+getInterface mods file = do
       mod <-
         return . fmap srcInfoSpan . fromParseResult =<<
         parseFile file
       let mExps = snd <$> processExports (moduleTable mod) mod
           exps = runIdentity $
-            evalModuleT mExps [] (error "retrieve") Map.empty
-      BS.writeFile out $ BS.fromString $ ppShow exps
+            evalModuleT mExps [] (error "retrieve") mods
+      return exps
 
 exportTests = do
+  libraryIface <- getInterface mempty "tests/exports/Library.hs"
+  let mods = Map.singleton (convertModuleName "Library") libraryIface
   testFiles <- find (return True) (extension ==? ".hs") "tests/exports"
-  return $ testGroup "exports" $ map exportTest testFiles
+  return $ testGroup "exports" $ map (exportTest mods) testFiles
