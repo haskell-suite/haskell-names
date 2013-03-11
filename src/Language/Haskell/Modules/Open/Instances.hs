a735276c958d983b4dce999b3be2ca8d2a12443f
Roman Cheplyaka <roma@ro-che.info>
2013-03-11 07:21:27 +0200

    Minor changes

--- src/Language/Haskell/Modules/Open/Instances.hs
+++ src/Language/Haskell/Modules/Open/Instances.hs
@@ -5,23 +5,22 @@ module Language.Haskell.Modules.Open.Instances where
 import Language.Haskell.Modules.Open.Base
 import Language.Haskell.Modules.Open.Derived ()
 import Data.Generics.SYB.WithClass.Basics
+import Data.Generics.SYB.WithClass.Instances ()
 import Language.Haskell.Exts.Annotated
-
--- XXX
 import qualified Data.Data as D
 
 instance (Data ResolvableD l, Sat (ResolvableD l), SrcInfo l, D.Data l) =>
   Sat (ResolvableD (Decl l)) where
-  dict = ResolvableD $ \(*) z e ->
+  dict = ResolvableD $ \(<|) z e ->
     case e of
       PatBind l pat mbType rhs mbWhere ->
         z (\l pat mbWhere mbType rhs -> PatBind l pat mbType rhs mbWhere)
-          * l >>= \k ->
-        k * pat >>= \k -> intro pat $
-        k * mbWhere >>= \k ->
-        k * mbType >>= \k ->
-        k * rhs
-      _ -> defaultRfoldl (*) z e
+          <| l >>= \k ->
+        k <| pat >>= \k -> intro pat $
+        k <| mbWhere >>= \k ->
+        k <| mbType >>= \k ->
+        k <| rhs
+      _ -> defaultRfoldl (<|) z e
 
 {-
 instance (Data ResolvableD l, Sat (ResolvableD l), SrcInfo l, D.Data l) =>
