d8bd0d4c2a5a6cd914213fc454b70101b576197d
Roman Cheplyaka <roma@ro-che.info>
2013-04-07 18:34:27 +0300

    Language.Haskell.Modules.Annotated

---
+++ src/Language/Haskell/Modules/Annotated.hs
@@ -0,0 +1,49 @@
+-- This module uses the open recursion interface
+-- ("Language.Haskell.Modules.Open") to annotate the AST with binding
+-- information.
+{-# LANGUAGE FlexibleContexts, MultiParamTypeClasses, ImplicitParams,
+    UndecidableInstances, OverlappingInstances, ScopedTypeVariables,
+    TypeOperators, GADTs #-}
+module Language.Haskell.Modules.Annotated
+  ( annotate
+  ) where
+
+import Language.Haskell.Modules.Types
+import Language.Haskell.Modules.Open.Base
+import Language.Haskell.Modules.Open.Instances
+import qualified Language.Haskell.Modules.GlobalSymbolTable as Global
+import qualified Language.Haskell.Modules.LocalSymbolTable as Local
+import Language.Haskell.Exts.Annotated
+import Data.Generics.Traversable
+import Data.Proxy
+import Data.Lens.Common
+import Data.Typeable
+import Control.Applicative
+
+-- This should be incorporated into Data.Typeable soon
+import Type.Eq
+
+annotate
+  :: forall a l .
+     (Resolvable (a (Scoped l)), Functor a, Typeable l)
+  => Scope -> a l -> a (Scoped l)
+annotate sc = annotateRec (Proxy :: Proxy l) sc . fmap None
+
+annotateRec
+  :: forall a l .
+     (Typeable l, Resolvable a)
+  => Proxy l -> Scope -> a -> a
+annotateRec _ sc a
+  | Just (Eq :: QName (Scoped l) :~: a) <- dynamicEq
+    = lookupValue (fmap sLoc a) sc <$ a
+
+lookupValue :: QName l -> Scope -> Scoped l
+lookupValue qn sc =
+  let l = ann qn in
+
+  either (\e -> ScopeError l e) id $
+  (LocalValue  l <$> (Local.lookupValue  qn $ getL lTable sc)) <|>
+  (GlobalValue l <$> (Global.lookupValue qn $ getL gTable sc))
+  where
+    x@Right{} <|> _ = x
+    _ <|> y = y
