2a882e1e4318a9a98144e83cc31c7bed920d37b7
Roman Cheplyaka <roma@ro-che.info>
2013-04-19 17:06:23 +0300

    Test case guards

--- tests/annotations/Case.hs
+++ tests/annotations/Case.hs
@@ -5,6 +5,12 @@ module Case where
 f x = case x of
   Right (a,b,c) -> a b c
   Left 0 -> 0
+  Left y
+    | u -> u
+    | let v = u, k <- v -> u
+    where
+      u = y
+      q = v -- not in scope
   Left y -> t a y -- NB: 'a' is intentionally unbound
     where
       t = y x
