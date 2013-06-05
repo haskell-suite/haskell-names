6a4168b2fee1a01cc2ed08e1b48c43c823ae4289
Roman Cheplyaka <roma@ro-che.info>
2013-06-05 11:24:41 +0300

    Make interface resolution functions return errors

--- src/Language/Haskell/Modules/Recursive.hs
+++ src/Language/Haskell/Modules/Recursive.hs
@@ -8,11 +8,13 @@ module Language.Haskell.Modules.Recursive
 import Data.Graph(stronglyConnComp, flattenSCC)
 import Data.Monoid
 import Data.Data (Data)
-import Control.Monad
+import qualified Data.Set as Set
+import Control.Monad hiding (forM_)
 import Control.Applicative
 import Language.Haskell.Exts.Annotated
 import Distribution.HaskellSuite.Modules
 import Data.Maybe
+import Data.Foldable
 
 import Language.Haskell.Modules.Types
 import Language.Haskell.Modules.SyntaxUtils
@@ -68,42 +70,47 @@ annotateModule mod@(Module lm mh os is ds) = do
   return $ Module lm' mh' os' is' ds'
 
 -- | Compute interfaces for a set of mutually recursive modules and write
--- the results to the cache
+-- the results to the cache. Return the set of import/export errors.
 findFixPoint
-  :: (Eq l, Data l, MonadModule m, ModuleInfo m ~ Symbols)
-  => [Module l] -> m ()
+  :: (Ord l, Data l, MonadModule m, ModuleInfo m ~ Symbols)
+  => [Module l] -> m (Set.Set (Error l))
 findFixPoint mods = go mods (map (const mempty) mods) where
   go mods syms = do
     forM_ (zip syms mods) $ \(s,m) -> insertInCache (getModuleName m) s
-    syms' <- forM mods $ \m -> do
+    (syms', errors) <- liftM unzip $ forM mods $ \m -> do
       (imp, impTbl) <- processImports $ getImports m
       let ownTbl = moduleTable m
           tbl = impTbl <> ownTbl
       (exp, syms) <- processExports tbl m
-      return syms
+      return (syms, foldMap getErrors imp <> foldMap getErrors exp)
     if syms' == syms
-      then return ()
+      then return $ mconcat errors
       else go mods syms'
 
 -- | 'computeInterfaces' takes a list of possibly recursive modules and
 -- computes the interface of each module. The computed interfaces are
 -- written into the @m@'s cache and are available to further computations
 -- in this monad.
+--
+-- Returns the set of import/export errors. Note that the interfaces are
+-- registered in the cache regardless of whether there are any errors, but
+-- if there are errors, the interfaces may be incomplete.
 computeInterfaces
-  :: (MonadModule m, ModuleInfo m ~ Symbols, Data l, SrcInfo l, Eq l)
-  => [Module l] -> m ()
+  :: (MonadModule m, ModuleInfo m ~ Symbols, Data l, SrcInfo l, Ord l)
+  => [Module l] -> m (Set.Set (Error l))
 computeInterfaces =
-  mapM_ findFixPoint . groupModules
+  liftM fold . mapM findFixPoint . groupModules
 
--- | Like 'computeInterfaces', but also return a list of interfaces, one
+-- | Like 'computeInterfaces', but also returns a list of interfaces, one
 -- per module and in the same order
 getInterfaces
-  :: (MonadModule m, ModuleInfo m ~ Symbols, Data l, SrcInfo l, Eq l)
-  => [Module l] -> m [Symbols]
+  :: (MonadModule m, ModuleInfo m ~ Symbols, Data l, SrcInfo l, Ord l)
+  => [Module l] -> m ([Symbols], Set.Set (Error l))
 getInterfaces mods = do
-  computeInterfaces mods
-  forM mods $ \mod ->
+  errs <- computeInterfaces mods
+  ifaces <- forM mods $ \mod ->
     let modName = getModuleName mod in
     fromMaybe (error $ msg modName) `liftM` lookupInCache modName
+  return (ifaces, errs)
   where
     msg modName = "getInterfaces: module " ++ modToString modName ++ " is not in the cache"
