7ba836e0ef8f1876ed1f7f36100dc39f0f84c50b
Roman Cheplyaka <roma@ro-che.info>
2013-09-09 19:12:24 +0300

    Add a test for record puns

---
+++ tests/annotations/Records.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE NamedFieldPuns, RecordWildcards #-}
+module Records where
+
+data A = A { b :: A, c :: A }
+
+undefined = undefined
+
+-- simple record pattern match
+f A { b = q } = q
+
+-- simple record update
+
+fu x = x { b = b x }
+
+-- pattern pun
+g A {b} = b
+
+-- top-level pattern pun
+A { tx } = undefined
+ty = tx
+
+-- expression pun
+tz = let b = 1 in A { b }
+
+-- mixed pattern
+h (A {b, c = (4, y)}) = (b, y)
+
+-- mixed expression
+tm = let b = 1; v = 2 in A { b, c = v }
+
+-- stripping module name
+-- (not supported by HSE yet)
+{- f A { M.b } = b -}
