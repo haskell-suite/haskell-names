fd6424cba90041c2018173e9f390f7a0f60e3ecd
Roman Cheplyaka <roma@ro-che.info>
2013-11-27 12:54:12 +0200

    Tests for export annotations

---
+++ tests/annotations/Export.hs
@@ -0,0 +1,9 @@
+module ImportExport
+  ( function -- EVar
+  , Newtype  -- EAbs
+  , DataType(..) -- EThingAll
+  , DataTypeWithSelectors(selector1) -- EThingWith
+  , module Prelude -- EModuleContents
+  ) where
+
+import Prelude
